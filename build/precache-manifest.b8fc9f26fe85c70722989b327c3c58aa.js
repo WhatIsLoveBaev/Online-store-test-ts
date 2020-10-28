b85779382befd0478851aa40aed1349640c435ca
WhatIsLove <z@z.ru>
2020-10-28 21:42:29 +0400

    gh-pages

---
+++ build/precache-manifest.b8fc9f26fe85c70722989b327c3c58aa.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a541498fa69f45d3195f083da20f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ec5577f90bba1e1bb5",
+    "url": "/static/css/main.cf600873.chunk.css"
+  },
+  {
+    "revision": "c54bb1995d8a74d11304",
+    "url": "/static/js/2.f45b6703.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f45b6703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ec5577f90bba1e1bb5",
+    "url": "/static/js/main.bc58a692.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1ba71ffaf0a02167a1d0d5c6b7ef0ab9",
+    "url": "/static/media/FuturaPT-Bold.1ba71ffa.eot"
+  },
+  {
+    "revision": "6dddddc0b4726becc525fc7794cfcf6b",
+    "url": "/static/media/FuturaPT-Bold.6dddddc0.ttf"
+  },
+  {
+    "revision": "9d2b3b49fdbaf67b82b3bdfb098479a7",
+    "url": "/static/media/FuturaPT-Bold.9d2b3b49.woff"
+  },
+  {
+    "revision": "010a2bdd5ca0642a0c2c346273eb0c3c",
+    "url": "/static/media/FuturaPT-BoldObl.010a2bdd.eot"
+  },
+  {
+    "revision": "1636d83d92a3f0fe3b6ceb2d6d704a77",
+    "url": "/static/media/FuturaPT-BoldObl.1636d83d.ttf"
+  },
+  {
+    "revision": "4c6a7ce3447d971988d33bb5b3894593",
+    "url": "/static/media/FuturaPT-BoldObl.4c6a7ce3.woff"
+  },
+  {
+    "revision": "0d987efe9bc0b858a7bc0367c2d5922c",
+    "url": "/static/media/FuturaPT-Book.0d987efe.ttf"
+  },
+  {
+    "revision": "ba7c7f34c672e9f406dd97a9a8f33dec",
+    "url": "/static/media/FuturaPT-Book.ba7c7f34.eot"
+  },
+  {
+    "revision": "c431096955795da262fc32297d7bfa8f",
+    "url": "/static/media/FuturaPT-Book.c4310969.woff"
+  },
+  {
+    "revision": "02738f97b87fd3e6979a5de80beb58bd",
+    "url": "/static/media/FuturaPT-BookObl.02738f97.ttf"
+  },
+  {
+    "revision": "9b7b47e123d554216079d5a218f761d9",
+    "url": "/static/media/FuturaPT-BookObl.9b7b47e1.eot"
+  },
+  {
+    "revision": "c1ad74d57bc1d09093c1c6b2ae6a9b34",
+    "url": "/static/media/FuturaPT-BookObl.c1ad74d5.woff"
+  },
+  {
+    "revision": "65ee0094094ad87a468482025e755626",
+    "url": "/static/media/FuturaPT-Demi.65ee0094.woff"
+  },
+  {
+    "revision": "779ccc41167f93f049dc96c2d75dd782",
+    "url": "/static/media/FuturaPT-Demi.779ccc41.eot"
+  },
+  {
+    "revision": "cc2af0787cb4b6579c8de6cdb7e0ec19",
+    "url": "/static/media/FuturaPT-Demi.cc2af078.ttf"
+  },
+  {
+    "revision": "35d92a04dae4b2d96203f223b57dfa85",
+    "url": "/static/media/FuturaPT-DemiObl.35d92a04.eot"
+  },
+  {
+    "revision": "4bbf82fafa4815892f66617c2d009284",
+    "url": "/static/media/FuturaPT-DemiObl.4bbf82fa.woff"
+  },
+  {
+    "revision": "f1c52f0223d3590a23ab46f1a842926b",
+    "url": "/static/media/FuturaPT-DemiObl.f1c52f02.ttf"
+  },
+  {
+    "revision": "b1b49131aa8c785f2efc0027c1063bf6",
+    "url": "/static/media/FuturaPT-ExtraBold.b1b49131.woff"
+  },
+  {
+    "revision": "c999cbc4fc7c2f563ac1c68072823706",
+    "url": "/static/media/FuturaPT-ExtraBold.c999cbc4.ttf"
+  },
+  {
+    "revision": "fc2acef4db3a0650bc24b7606a359b41",
+    "url": "/static/media/FuturaPT-ExtraBold.fc2acef4.eot"
+  },
+  {
+    "revision": "05a35faaff84442e557157df7597c995",
+    "url": "/static/media/FuturaPT-ExtraBoldObl.05a35faa.ttf"
+  },
+  {
+    "revision": "97b9bb7be95ab0770815093e5cbd5a5f",
+    "url": "/static/media/FuturaPT-ExtraBoldObl.97b9bb7b.woff"
+  },
+  {
+    "revision": "f803a78c4297e6a664aea8be29ebc3be",
+    "url": "/static/media/FuturaPT-ExtraBoldObl.f803a78c.eot"
+  },
+  {
+    "revision": "6f586d0063dc5635a913782659c40a0d",
+    "url": "/static/media/FuturaPT-Heavy.6f586d00.eot"
+  },
+  {
+    "revision": "8b9416a7a903a8734f22e4fd043f74e9",
+    "url": "/static/media/FuturaPT-Heavy.8b9416a7.ttf"
+  },
+  {
+    "revision": "ec50ee1633d1684fd0431d772c35cca8",
+    "url": "/static/media/FuturaPT-Heavy.ec50ee16.woff"
+  },
+  {
+    "revision": "1a9fdf9ee64fb3b54e42143e7485015c",
+    "url": "/static/media/FuturaPT-HeavyObl.1a9fdf9e.woff"
+  },
+  {
+    "revision": "1ee2335b649ecbea72fa3c718f113259",
+    "url": "/static/media/FuturaPT-HeavyObl.1ee2335b.eot"
+  },
+  {
+    "revision": "505ba927ca14f78ee751f16acc4fb591",
+    "url": "/static/media/FuturaPT-HeavyObl.505ba927.ttf"
+  },
+  {
+    "revision": "048e9ded599ca071771730e1b7532fda",
+    "url": "/static/media/FuturaPT-Light.048e9ded.eot"
+  },
+  {
+    "revision": "92aa0d7878fe4d8d55dab98aae0796e5",
+    "url": "/static/media/FuturaPT-Light.92aa0d78.woff"
+  },
+  {
+    "revision": "c7f1106be884b5027c6d93deec8ca00a",
+    "url": "/static/media/FuturaPT-Light.c7f1106b.ttf"
+  },
+  {
+    "revision": "3e5135bd678f5b5cb4c6b32926e46dd0",
+    "url": "/static/media/FuturaPT-LightObl.3e5135bd.ttf"
+  },
+  {
+    "revision": "fda2a31dec14917d8ac99516e0ba2ce7",
+    "url": "/static/media/FuturaPT-LightObl.fda2a31d.woff"
+  },
+  {
+    "revision": "fe5cacfaf1f974f1372cf34f2e3efb98",
+    "url": "/static/media/FuturaPT-LightObl.fe5cacfa.eot"
+  },
+  {
+    "revision": "540c81bc4ca37ae1efe4b60de51e753f",
+    "url": "/static/media/FuturaPT-Medium.540c81bc.ttf"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/static/media/FuturaPT-Medium.995c64e1.woff"
+  },
+  {
+    "revision": "a61e39ce10e6f9114cbca70f85fbb10b",
+    "url": "/static/media/FuturaPT-Medium.a61e39ce.eot"
+  },
+  {
+    "revision": "3e4a05e49a29c1e75d151301e843c152",
+    "url": "/static/media/FuturaPT-MediumObl.3e4a05e4.woff"
+  },
+  {
+    "revision": "74ff1c43cf8073ff55cdd691d552ce8c",
+    "url": "/static/media/FuturaPT-MediumObl.74ff1c43.eot"
+  },
+  {
+    "revision": "f90c9809d9d4b016f83e11504701cf5d",
+    "url": "/static/media/FuturaPT-MediumObl.f90c9809.ttf"
+  },
+  {
+    "revision": "0fdc90873c9fd200edb700cdfb222a60",
+    "url": "/static/media/FuturaPTCond-Bold.0fdc9087.eot"
+  },
+  {
+    "revision": "3cb6189ae19ed713483961702f68f4c2",
+    "url": "/static/media/FuturaPTCond-Bold.3cb6189a.woff"
+  },
+  {
+    "revision": "7ede65733dacdd6975db06e7e2f06d84",
+    "url": "/static/media/FuturaPTCond-Bold.7ede6573.ttf"
+  },
+  {
+    "revision": "02675de4a0fa6c5a05100fdbb30b23ed",
+    "url": "/static/media/FuturaPTCond-BoldObl.02675de4.woff"
+  },
+  {
+    "revision": "25b7effc35f37100ab128341faf7ca6c",
+    "url": "/static/media/FuturaPTCond-BoldObl.25b7effc.eot"
+  },
+  {
+    "revision": "e24d9a9d51b6ca7e400c97d9a7e93d3b",
+    "url": "/static/media/FuturaPTCond-BoldObl.e24d9a9d.ttf"
+  },
+  {
+    "revision": "bb8eaa812c606ef76a65d0ae1cb70ff6",
+    "url": "/static/media/FuturaPTCond-Book.bb8eaa81.woff"
+  },
+  {
+    "revision": "caf21e58fd6b6f3cd983abd7d3172871",
+    "url": "/static/media/FuturaPTCond-Book.caf21e58.eot"
+  },
+  {
+    "revision": "f85738dcbde9166d12d053ba6ae5a01b",
+    "url": "/static/media/FuturaPTCond-Book.f85738dc.ttf"
+  },
+  {
+    "revision": "5b4ae46cd09efbbdef0b2cfa35b35cff",
+    "url": "/static/media/FuturaPTCond-BookObl.5b4ae46c.eot"
+  },
+  {
+    "revision": "73b4a75b54f56ff6535b3426f7724139",
+    "url": "/static/media/FuturaPTCond-BookObl.73b4a75b.woff"
+  },
+  {
+    "revision": "c94002c7e5e35db0b4eb473ddaf75bc6",
+    "url": "/static/media/FuturaPTCond-BookObl.c94002c7.ttf"
+  },
+  {
+    "revision": "24f1af89556b236a6b84fdd0dff80d9d",
+    "url": "/static/media/FuturaPTCond-ExtraBold.24f1af89.ttf"
+  },
+  {
+    "revision": "96bb9a37ecdf54a097b6582d1db180e5",
+    "url": "/static/media/FuturaPTCond-ExtraBold.96bb9a37.woff"
+  },
+  {
+    "revision": "b6fa9ebbd1d46c6c2d163fabb382c42a",
+    "url": "/static/media/FuturaPTCond-ExtraBold.b6fa9ebb.eot"
+  },
+  {
+    "revision": "15401b2b2c77b17d695e53d5139f3652",
+    "url": "/static/media/FuturaPTCond-ExtraBoldObl.15401b2b.ttf"
+  },
+  {
+    "revision": "b34ff838d14d2a77c89bd6bf7058e682",
+    "url": "/static/media/FuturaPTCond-ExtraBoldObl.b34ff838.eot"
+  },
+  {
+    "revision": "e80bb0fbc9fbe025d9e559c234f1b3bb",
+    "url": "/static/media/FuturaPTCond-ExtraBoldObl.e80bb0fb.woff"
+  },
+  {
+    "revision": "43e5a7cd73d2af6c983c5459fc6d3a25",
+    "url": "/static/media/FuturaPTCond-Medium.43e5a7cd.woff"
+  },
+  {
+    "revision": "47082d4680c22446480469a10e08bf8b",
+    "url": "/static/media/FuturaPTCond-Medium.47082d46.eot"
+  },
+  {
+    "revision": "a2a7debca6b8988db5f0dbe382789707",
+    "url": "/static/media/FuturaPTCond-Medium.a2a7debc.ttf"
+  },
+  {
+    "revision": "07cab374c27a0df130764a3aac4785dd",
+    "url": "/static/media/FuturaPTCond-MediumObl.07cab374.ttf"
+  },
+  {
+    "revision": "5f2bef48cbbdcecdcec5109ebc48a195",
+    "url": "/static/media/FuturaPTCond-MediumObl.5f2bef48.eot"
+  },
+  {
+    "revision": "692f3bee4210044d42d4dc9df4d31cd3",
+    "url": "/static/media/FuturaPTCond-MediumObl.692f3bee.woff"
+  },
+  {
+    "revision": "1a653f3126cc0168240b03ec4d509a9b",
+    "url": "/static/media/camera.1a653f31.jpg"
+  },
+  {
+    "revision": "8392114f8310326712e02381c3f60783",
+    "url": "/static/media/iphone.8392114f.jpg"
+  },
+  {
+    "revision": "d9d50fbf29876ffcdd6a0b598679ade9",
+    "url": "/static/media/iphone_video.d9d50fbf.mp4"
+  },
+  {
+    "revision": "8fca6199efb6efbc8219ac4b263b8160",
+    "url": "/static/media/keyboard.8fca6199.jpg"
+  },
+  {
+    "revision": "06684a8b9162bd7ceae69699062f209c",
+    "url": "/static/media/monitor.06684a8b.jpg"
+  },
+  {
+    "revision": "6c052b7693bfae3abef5378ab795dafa",
+    "url": "/static/media/phone.6c052b76.jpg"
+  },
+  {
+    "revision": "4acc56db4376fc526c9f76594c7aa463",
+    "url": "/static/media/samsung.4acc56db.jpg"
+  },
+  {
+    "revision": "4bbf68d0ca9acf350075b54f65483689",
+    "url": "/static/media/samsung_video.4bbf68d0.mp4"
+  },
+  {
+    "revision": "9127633dca8e54a2a2ffdd8a08a13678",
+    "url": "/static/media/tablet.9127633d.jpg"
+  },
+  {
+    "revision": "ff4a7fdb9a09b0b874895eb8ca707737",
+    "url": "/static/media/xiaomi.ff4a7fdb.jpg"
+  }
+]);
